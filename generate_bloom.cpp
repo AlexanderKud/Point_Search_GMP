e3adb2efae3be748b1911de2137013930527da28
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-05-13 22:29:59 +0300

    Update generate_bloom.cpp

--- generate_bloom.cpp
+++ generate_bloom.cpp
@@ -124,9 +124,9 @@ auto main() -> int {
     using filter = boost::bloom::filter<std::string, 32>;
     uint64_t n_elements = uint64_t(pow(2, block_width) * 1.0);
     double error = 0.0000000001;
-    int n_cores = 4;
-    uint64_t count = uint64_t(pow(2, block_width) / n_cores);
-    mpz_class add_key; 
+    int n_cores = 4;  //actual number of processing cores but equal to some power of two value(2,4,8,16,32,64,...) divided by 2
+    uint64_t count = uint64_t(pow(2, block_width) / n_cores); // actual cores = 8  8 / 2 = 4 cores for each lambda function
+    mpz_class add_key;                                        // should be some power of two to evenly divide the space between threads
     add_key = count;
     Point Add_Point = secp256k1->ScalarMultiplication(add_key);
     
