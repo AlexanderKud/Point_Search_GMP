59d40b3107c406409c073542ebf68585f0f6cf12
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-05-17 23:57:39 +0300

    update

--- point_search.cpp
+++ point_search.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <algorithm>
 #include <thread>
-#include <cmath>
 #include <gmpxx.h>
 #include <gmp.h>
 
@@ -15,45 +14,10 @@
 
 using namespace std;
 
-inline uint64_t str_to_uint64(std::string const& value) {
-  uint64_t result = 0;
-  size_t const length = value.size();
-  switch (length) {
-    case 20:    result += (value[length - 20] - '0') * 10000000000000000000ULL;
-    case 19:    result += (value[length - 19] - '0') * 1000000000000000000ULL;
-    case 18:    result += (value[length - 18] - '0') * 100000000000000000ULL;
-    case 17:    result += (value[length - 17] - '0') * 10000000000000000ULL;
-    case 16:    result += (value[length - 16] - '0') * 1000000000000000ULL;
-    case 15:    result += (value[length - 15] - '0') * 100000000000000ULL;
-    case 14:    result += (value[length - 14] - '0') * 10000000000000ULL;
-    case 13:    result += (value[length - 13] - '0') * 1000000000000ULL;
-    case 12:    result += (value[length - 12] - '0') * 100000000000ULL;
-    case 11:    result += (value[length - 11] - '0') * 10000000000ULL;
-    case 10:    result += (value[length - 10] - '0') * 1000000000ULL;
-    case  9:    result += (value[length -  9] - '0') * 100000000ULL;
-    case  8:    result += (value[length -  8] - '0') * 10000000ULL;
-    case  7:    result += (value[length -  7] - '0') * 1000000ULL;
-    case  6:    result += (value[length -  6] - '0') * 100000ULL;
-    case  5:    result += (value[length -  5] - '0') * 10000ULL;
-    case  4:    result += (value[length -  4] - '0') * 1000ULL;
-    case  3:    result += (value[length -  3] - '0') * 100ULL;
-    case  2:    result += (value[length -  2] - '0') * 10ULL;
-    case  1:    result += (value[length -  1] - '0');
-  }
-  return result;
-}
-
-vector<uint64_t> break_down_to_pow10(uint64_t num) {
-    vector<uint64_t> nums;
-    string stri = to_string(num);
-    int num_len = stri.length() - 2;
-    for (int pw = num_len; pw >= 0; pw--) { nums.push_back(uint64_t(pow(10, pw))); }
-    return nums;
-}
-
 auto main() -> int {
 
     Secp256k1 *secp256k1 = new Secp256k1(); secp256k1->Init();
+    int cpuCores = 4; // actual number of processing cores divided by 2
     
     mpz_class pk; pk = 1;
     vector<mpz_class> S_table;
@@ -130,7 +94,7 @@ auto main() -> int {
         stride = pow(2, block_width);
         stride_point = secp256k1->ScalarMultiplication(stride);
         
-        int n_cores = 4;
+        int n_cores = cpuCores;
         
         mpz_class offset_Step, vector_Num;
         mpz_fdiv_q_ui(offset_Step.get_mpz_t(), S_table[range_start - 2].get_mpz_t(),  n_cores);
@@ -309,7 +273,7 @@ auto main() -> int {
         stride = pow(2, block_width);
         stride_point = secp256k1->ScalarMultiplication(stride);
         
-        int n_cores = 4;
+        int n_cores = cpuCores;
         
         mpz_class offset_Step, vector_Num;
         mpz_fdiv_q_ui(offset_Step.get_mpz_t(), S_table[range_start - 2].get_mpz_t(),  n_cores);
